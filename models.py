b0cf08e37106499c4a44a1082a2006192fb33153
Bryan Aguilar <baguilar@technisys.com>
2025-12-05 14:55:47 -0500

    🧹 chore: Add model Plan and Customer Plan to represent a relationship many to many

--- models.py
+++ models.py
@@ -1,33 +1,48 @@
 from pydantic import BaseModel
 from sqlmodel import SQLModel, Field, Relationship
 
+
 class CustomerBase(SQLModel):
     name: str = Field(default=None)
     description: str | None = Field(default=None)
     email: str = Field(default=None)
     age: int = Field(default=None)
 
+
 class CustomerCreate(CustomerBase):
     pass
+
 
 class CustomerUpdate(CustomerBase):
     pass
 
+
+class CustomerPlan(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    plan_id: int = Field(foreign_key="plan.id")
+    customer_id: int = Field(foreign_key="customer.id")
+
+
 class Customer(CustomerBase, table=True):
-    id : int | None = Field(default=None, primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
     transactions: list["Transaction"] = Relationship(back_populates="customer")
+    plans: list["Plan"] = Relationship(back_populates="customers", link_model=CustomerPlan)
+
 
 class TransactionBase(SQLModel):
     amount: int = Field(default=None)
     description: str = Field(default=None)
 
+
 class TransactionCreate(TransactionBase):
-    customer_id : int = Field(foreign_key="customer.id")
+    customer_id: int = Field(foreign_key="customer.id")
+
 
 class Transaction(TransactionBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    customer_id : int = Field(foreign_key="customer.id")
+    customer_id: int = Field(foreign_key="customer.id")
     customer: Customer = Relationship(back_populates="transactions")
+
 
 class Invoice(BaseModel):
     id: int
@@ -38,3 +53,11 @@ class Invoice(BaseModel):
     @property
     def total_amount(self) -> int:
         return sum(transaction.amount for transaction in self.transactions)
+
+
+class Plan(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(default=None)
+    price: int = Field(default=None)
+    description: str = Field(default=None)
+    customers: list["Customer"] = Relationship(back_populates="plans", link_model=CustomerPlan)
